fcdb87893c3068800aa895fd0220bcdbd82a3d45
ElMoucheh <williambizien@yahoo.com>
2024-11-14 13:33:50 +0100

    upadate modal

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Modal from './lib/modal';
+import './lib/modal';
+
+export default Modal;
+
+export { Modal };
---
+++ src/lib/modal.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import './modal.scss';
+
+const Modal = ({ isOpen, onClose, children}) => {
+  useEffect(() => {
+    const handleEsc = (event) => {
+      if (event.key === 'Escape' && isOpen) {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleEsc);
+    return () => window.removeEventListener('keydown', handleEsc);
+  }, [isOpen, onClose]);
+
+  const handleClickOutside = (e) => {
+    if (e.target.className === 'modal-overlay') {
+      onClose();
+    }
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay" onClick={handleClickOutside}>
+      <div className="modal">
+        <div className="modal-content">
+            <button className="close-modal" onClick={onClose} aria-label="Close modal">X</button>
+            {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
